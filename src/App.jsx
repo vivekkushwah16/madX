49e3fe5ee9fabaf510f82f3439f5124415164327
Vivek kushwah <46090818+vivekkushwah16@users.noreply.github.com>
2021-09-15 21:45:15 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Banner from './Components/Banner';
+import {Cards} from "./Components/Cards"
+import NavBar from './Components/NavBar';
+
+
+export function App() {
+    return (
+        <>
+           <Banner />
+           <NavBar/>
+           <Cards />
+        </>
+    )
+}
+
